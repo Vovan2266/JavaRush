0e62f3a5799c0b37fa44ceef6800c009d8a3ad13
Vovan2266 <Vovan.bryant24@yandex.ru>
2017-03-22 17:33:18 +0300

    22 марта

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0714/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task07.task0714;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Слова в обратном порядке
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < 5; i++){
+            String s = reader.readLine();
+            list.add(s);
+
+        }
+            list.remove(2);
+        for(int i = 0; i < list.size(); i++){
+            int j = list.size() - i - 1;
+            System.out.println(list.get(j));
+        }
+    }
+}
+
+
---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0719/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task07.task0719;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Вывести числа в обратном порядке
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i = 0; i < 10; i++){
+            int s = Integer.parseInt(reader.readLine());
+            list.add(s);
+        }
+        for(int i = 0; i < list.size(); i++){
+            int j = list.size() - i - 1;
+            System.out.println(list.get(j));
+        }
+    }
+}
